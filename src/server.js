f751e29e48147e62fc309113c666c3b48fd2da87
hopeKiddo <maglaquijohnhopefuedan@gmail.com>
2024-09-29 17:22:23 +0800

    server

--- src/server.js
+++ src/server.js
@@ -14,17 +14,17 @@ const port = 5000;
 const helmet = require('helmet');
 
 app.use(bodyParser.json());
-const allowedOrigins = ['https://skinmiso.ca', 'http://localhost:3000', 'https://skinmiso.vercel.app'];
+const allowedOrigins = ['http://localhost:3000'];
 app.use(cors({
-  // origin: (origin, callback) => {
-  //   // Allow requests with no origin, like mobile apps or curl requests
-  //   if (!origin) return callback(null, true);
-  //   if (allowedOrigins.includes(origin)) {
-  //     return callback(null, true);
-  //   } else {
-  //     return callback(new Error('Not allowed by CORS'));
-  //   }
-  // },
+  origin: (origin, callback) => {
+    // Allow requests with no origin, like mobile apps or curl requests
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-access-token', 'X-Requested-With','Accept'],
   credentials: true, // Allow credentials (cookies, etc.) in CORS requests
