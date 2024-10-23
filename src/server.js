8c8c4e0b4994b9ea2dcb9c579e16b1a69bc1b68b
HopeKiddo <maglaquijohnhopefuedan@gmail.com>
2024-10-23 22:01:31 +0800

    asd

--- src/server.js
+++ src/server.js
@@ -271,14 +271,12 @@ app.post("/api/register-acc", limiter, [
 
     const insertSql = "INSERT INTO sk_customer_credentials (user_customerID, user_mobileno, user_email, user_username, user_password, user_role, user_activity, user_loginSession) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
     const [insertResult] = await db.query(insertSql, [customerID, mobileno, email, username, hash_pass, userRole, activity, loginSession]);
-    console.log(insertResult);
 
-    console.log(customerID,jwtSecret);
     if (insertResult.affectedRows > 0) {
       const authToken = jwt.sign({ customerID }, jwtSecret, { expiresIn: "7d" });
-      res.status(200).json({ success: true, message: "Customer registered successfully", token: authToken, loginSession });
+      return res.status(200).json({ success: true, message: 'Customer successfully registered', loginSession, token: authToken });
     } else {
-      res.status(500).json({ success: false, message: 'Error registering user' });
+      return res.status(500).json({ success: false, message: 'Error registering user' });
     }
   } catch (error) {
     console.error(error.message); // Log only the error message
