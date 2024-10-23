335547e47aa9113d099b9bed04ec8f454f04c830
HopeKiddo <maglaquijohnhopefuedan@gmail.com>
2024-10-23 21:57:31 +0800

    a

--- src/server.js
+++ src/server.js
@@ -273,8 +273,8 @@ app.post("/api/register-acc", limiter, [
     const [insertResult] = await db.query(insertSql, [customerID, mobileno, email, username, hash_pass, userRole, activity, loginSession]);
     console.log(insertResult);
 
+    console.log(customerID,jwtSecret);
     if (insertResult.affectedRows > 0) {
-      console.log(customerID,jwtSecret);
       const authToken = jwt.sign({ customerID }, jwtSecret, { expiresIn: "7d" });
       res.status(200).json({ success: true, message: "Customer registered successfully", token: authToken, loginSession });
     } else {
